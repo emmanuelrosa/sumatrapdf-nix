fb7d0bb6a9d996e6733855663910f6e964249579
Emmanuel Rosa <emmanuelrosa@protonmail.com>
2022-01-27 12:04:45 -0500

    open txt files with xdg-open

--- sumatrapdf.nix
+++ sumatrapdf.nix
@@ -11,6 +11,11 @@
   # The default settings used if user doesn't already have a settings file.
   # Tabs are disabled because they lead to UI issues when using Wine.
   defaultSettings = ./SumatraPDF-settings.txt;
+
+  # This registry file sets winebrowser (xdg-open) as the default handler for
+  # text files, instead of Wine's notepad.
+  # Selecting "Settings -> Advanced Options" should then use xdg-open to open the SumatraPDF config file.
+  txtReg = ./txt.reg;
 in mkWindowsApp rec {
   inherit wine;
 
@@ -79,6 +84,7 @@ in mkWindowsApp rec {
   # You need to set up symlinks for any files/directories that need to be persisted.
   # To figure out what needs to be persisted, take at look at $(dirname $WINEPREFIX)/upper
   winAppRun = ''
+    regedit ${txtReg}
     wine "$WINEPREFIX/drive_c/${pname}/SumatraPDF-${version}-64.exe" "$ARGS"
   '';
 
